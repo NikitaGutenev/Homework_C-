1a48eaec3484da37a1302737f324e27bf1fa8e5d
Nikita Gutenev <gutenev229@gmail.com>
2022-10-01 23:24:04 +0300

    сделал решение 4 задачи

---
+++ homework_C#/task4.cs
@@ -0,0 +1,13 @@
+// Console.WriteLine("Введите первое число: ");
+// int max = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите второе число: ");
+// int b = int.Parse(Console.ReadLine()!);
+// Console.WriteLine("Введите третье число: ");
+// int c = int.Parse(Console.ReadLine()!);
+// if (b > max) {
+//     max = b;
+// }
+// if (c > max) {
+//     max = c;
+// }
+// Console.WriteLine("max = " + max);
